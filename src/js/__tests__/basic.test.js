c5cb178f639a79cab8ec4d9c80b3e630812906e4
Yanius27 <zhzh-1987@mail.ru>
2023-08-14 19:44:05 +0700

    1 ex done

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -4,7 +4,7 @@ test.each([
   ['Маг', 90, 'healthy'],
   ['Варвар', 50, 'wounded'],
   ['Вор', 12, 'critical'],
-  ['no', 130, 0],
+  ['_', 130, 0],
 ])(
   ('should return correct health level for %s with %i health'),
   (a, b, expected) => {
